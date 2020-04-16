628a01dd02c86452fb90c6de4b22d0c4d2a48c7b
Fraser Cook <fraserc6@gmail.com>
2020-04-16 17:44:03 +0100

    FontAwesome config ordering

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../css/styles.css';
 
 import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 config.autoAddCss = false
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
