08096198af142aa01050175deeac23ffe1b019ed
Fraser Cook <fraserc6@gmail.com>
2020-04-20 14:47:47 +0100

    Postcss/Purgecss with Fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
 import '../css/styles.css';
 
-import { config } from '@fortawesome/fontawesome-svg-core'
-config.autoAddCss = false
-import '@fortawesome/fontawesome-svg-core/styles.css'
-
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
 }
