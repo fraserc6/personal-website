8232caca3b79b7e0a0576bfc62e9e4a560e6de9d
Fraser Cook <fraserc6@gmail.com>
2020-04-23 18:17:36 +0100

    Meta viewport

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Fraser Cook - Software Developer / UX Designer</title>
         <link rel="icon" href="/favicon.ico" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
 	  <div className="flex flex-col flex-grow min-h-screen">
 		<div className="flex-none bg-white">
